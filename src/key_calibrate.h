cb992db69116d3d2b1d41005d874188bf3902324
qibebt-bioinfo <46369225+qibebt-bioinfo@users.noreply.github.com>
2020-09-23 12:12:37 +0800

    Update key_calibrate.h

--- src/key_calibrate.h
+++ src/key_calibrate.h
@@ -100,7 +100,7 @@ int _Key_Calibrate::Train(float ** ref_abd, float ** target_abd, int pair_count)
 	
 	//vector <_OLS> pos_OLS (Key_dim, _OLS());
 	//vector <_OLS> neg_OLS (Key_dim, _OLS());
-	cout << "Meta-Apo Training Starts" << endl;
+	
 	vector <_OLS> this_model (Key_dim, _OLS());
 	
 	for (int i = 0; i < pair_count; i ++){
@@ -153,14 +153,14 @@ int _Key_Calibrate::Train(float ** ref_abd, float ** target_abd, int pair_count)
 	}
 	
 	Model_OLS = this_model;
-	cout << "Meta-Apo Training Finished" << endl;
+	
 	return 0;	
 }
 
 void _Key_Calibrate::Calibrate(float * abd, float * cal_abd){
 	
 	//norm
-	cout << "Meta-Apo Calibration Starts" << endl;
+	
 	float abd_sum = 0;
 	for (int i = 0; i < Key_dim; i ++){
         cal_abd[i] = abd[i];
@@ -185,7 +185,7 @@ void _Key_Calibrate::Calibrate(float * abd, float * cal_abd){
 	    //debug
 	    //cout << cal_abd[i] << "\t" <<  Model_OLS[i].Get_N() << "\t" << Model_OLS[i].Get_Beta_0() << "\t" << Model_OLS[i].Get_Beta_1() << endl;
     }
-	cout << "Meta-Apo Calibration Finished" << endl;
+	
 }
 
 #endif
